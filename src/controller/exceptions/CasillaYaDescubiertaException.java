46180d382aead8e4b99158edd523643edb2ab8d9
Edmundo Zea <erickzea45@gmail.com>
2026-06-05 01:16:48 -0400

    Add files via upload

---
+++ src/controller/exceptions/CasillaYaDescubiertaException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class CasillaYaDescubiertaException extends Exception {
+
+    public CasillaYaDescubiertaException(String mensaje) {
+        super(mensaje);
+    }
+}
